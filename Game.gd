a04b626346bb49181a56e452166c4e5931893c18
Kevin M. Smith <ksmith.nop@gmail.com>
2020-07-11 13:38:20 -0400

    Local y-axis rotation based on mouse moving left and right.

--- Game.gd
+++ Game.gd
@@ -1,16 +1,19 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event.is_action_pressed("lmb"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			get_tree().set_input_as_handled()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("enter"):
+		get_tree().reload_current_scene()
--- Player.gd
+++ Player.gd
@@ -5,6 +5,8 @@ extends KinematicBody
 var velocity: Vector3
 var gravity = Vector3(0, -40, 0)
 var speed = 20
+var jump = 20
+export var mouse_sensitivity = 0.002 # radians / pixel
 
 onready var planet = $"../Planet"
 
@@ -22,6 +24,23 @@ func local_input_direction():
 	
 	return dir.normalized()
 
+func draw_velocity():
+	var offset = Vector3.UP * 4
+	var begin = to_global(offset)
+	var end = to_global(velocity.normalized() + offset)
+	
+	$"/root/DebugDraw".reset($Debug)
+	#$"/root/DebugDraw".global_line($Debug, global_transform.origin + shift, to_global(velocity) + shift)
+	$"/root/DebugDraw".global_line($Debug, begin, end)
+
+func _unhandled_input(event):
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			var angle = -event.relative.x * mouse_sensitivity
+			#rotate_y(-event.relative.x * mouse_sensitivity)
+			
+			rotate_object_local(Vector3.UP, angle)
+
 func _physics_process(delta):
 	var world_up = (global_transform.origin - planet.global_transform.origin).normalized()
 	var world_down = (planet.global_transform.origin - global_transform.origin).normalized()
@@ -34,16 +53,27 @@ func _physics_process(delta):
 	#dir = (to_global(dir) - global_transform.origin).normalized()
 	#velocity += dir # The problem is that we continuously add but never reset
 
-
+	# "velocity" is always in local space.
 	velocity.x = dir.x * speed
 	velocity.z = dir.z * speed
-	velocity.y += -20 * delta
+	#velocity.y += -20 * delta
+	#velocity.y = 0
+	velocity.y = -20
 	
+	if Input.is_action_pressed("jump"):
+		velocity.y = jump
+	
+	# Convert "velocity" to global space because that's what move_and_slide() expects.
 	var global_velocity = to_global(velocity) - global_transform.origin
 	
-	global_velocity = move_and_slide(global_velocity, Vector3.UP, true)
+	global_velocity = move_and_slide(global_velocity, world_up, true)
+	#global_velocity = move_and_slide(global_velocity, -global_transform.basis.y, true)
 	
-	velocity = to_local(global_velocity) - transform.origin
+	# Update local "velocity" to account for changes move_and_slide() made.
+	velocity = to_local(global_transform.origin + global_velocity) - transform.origin
+	
+	print(velocity)
+	#draw_velocity()
 	
 	#velocity = move_and_slide_with_snap(velocity, Vector3.DOWN*2, Vector3.UP, true)
 	#velocity = move_and_slide(velocity, Vector3.UP, true)
