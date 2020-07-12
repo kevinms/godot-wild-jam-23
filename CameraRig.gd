ca887596769a266f67fcfa7945dadfd330b9b978
Kevin M. Smith <ksmith.nop@gmail.com>
2020-07-12 19:10:20 -0400

    Added a crosshair and attempting to line up the 3rd person camera in a way that feels decent.

--- CameraRig.gd
+++ CameraRig.gd
@@ -6,6 +6,9 @@ export var zoom_sensitivity = 0.1
 onready var min_dist = $Camera.translation.normalized() * 5
 onready var max_dist = $Camera.translation.normalized() * 20
 
+func _ready():
+	$Camera.look_at(global_transform.origin, Vector3.UP)
+
 func _unhandled_input(event):
 	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 		return
@@ -21,3 +24,7 @@ func _unhandled_input(event):
 		if event.button_index == BUTTON_WHEEL_DOWN:
 			var camera_pos = $Camera.transform.origin
 			$Camera.transform.origin = camera_pos.move_toward(max_dist, camera_pos.length() * zoom_sensitivity)
+
+func _process(delta):
+	var screen_center = get_viewport().size / 2
+	$Camera/Crosshair.position = screen_center
