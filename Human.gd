284e813b7e5e215a6c500b5f8a21861522550ed8
Kevin M. Smith <ksmith.nop@gmail.com>
2020-07-12 14:58:43 -0400

    Change human movement to randomly jump in a direction.

--- Human.gd
+++ Human.gd
@@ -1,55 +1,45 @@
 extends KinematicBody
 
-var max_speed = 50
-var speed = 1000
-var jump = 20
-var gravity = 100
+var speed = 1
+var jump = 2
+var gravity = 2
 
-#onready var planet = $"../Planet"
 export(NodePath) var planet_path
 onready var planet = get_node(planet_path)
 
 var global_velocity = Vector3()
+var jumping = false
 
 func _physics_process(delta):
-	
-	rotate_object_local(Vector3.UP, 0.01)
+	# Jump
+	if is_on_floor() and randf() < 0.01:
+		# Always jump up
+		global_velocity = global_transform.basis.y * jump
+		jumping = true
+		
+		# Determine the direction
+		var forward = -global_transform.basis.z.normalized()
+		var new_dir = forward.rotated(global_transform.basis.y, rand_range(0, 2*PI))
+		global_velocity += new_dir * speed
 
 	var accel = Vector3()
-	
-	# Move forward
-	#if randf() < 0.4:
-	#	accel += global_transform.basis.z * speed * delta
-	
-	#var forward_vector = global_velocity.project(global_transform.basis.z)
-	#if forward_vector.length() > max_speed:
-	#	global_velocity -= forward_vector.normalized() * (forward_vector.length() - max_speed)
-	
+
 	# Gravity
 	accel += -global_transform.basis.y * gravity * delta
 	
 	# Apply acceleration
 	global_velocity += accel * delta
 	
-	# Jump
-	#if randf() < 0.1 and is_on_floor():
-		# We want the jump to initially cancel out all force due to gravity
-		
-		# Project our velocity onto the local Y axis
-		# This gets the component of velocity in the direction of gravity
-		#var scalar_projection = global_velocity.dot(-global_transform.basis.y) / global_velocity.length()
-		#var vector_projection = -global_transform.basis.y * scalar_projection
-		
-		# Subtract that component from the velocity to cancel out all gravity
-		#global_velocity -= vector_projection
-		#global_velocity += global_transform.basis.y * jump
-	
 	var world_up = (global_transform.origin - planet.global_transform.origin).normalized()
 	
-	global_velocity = move_and_slide_with_snap(global_velocity, -world_up*2, world_up, true)
+	if jumping:
+		global_velocity = move_and_slide(global_velocity, world_up, true)
+	else:
+		global_velocity = move_and_slide_with_snap(global_velocity, -world_up*2, world_up, true)
 	
 	if get_slide_count() > 0:
-		print("Collided with planet... hopefully")
+		jumping = false
+		global_velocity = Vector3()
 	
 	var xform = align_with_y(global_transform, world_up)
 	global_transform = global_transform.interpolate_with(xform, 0.2)
@@ -59,5 +49,5 @@ func align_with_y(xform, new_y):
 	xform.basis.y = new_y
 	xform.basis.x = -xform.basis.z.cross(new_y)
 	xform.basis = xform.basis.orthonormalized()
-	#TODO: reset scale
+	#TODO: Set scale back to before orthonormalized()?
 	return xform
