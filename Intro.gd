73dc6cf12c128d456f27567196b65364c667304c
Kevin M. Smith <ksmith.nop@gmail.com>
2020-07-17 14:24:30 -0400

    Except for the missile firing queue, the intro is complete! OMG!

--- Intro.gd
+++ Intro.gd
@@ -13,23 +13,22 @@ func _ready():
 	$"Death/alien-04/AnimationPlayer".play("jump")
 	
 	$"NeverAgain/prop-text-7-never-again/AnimationPlayer".play("never-again-pulse")
-	
-const ST_FIRST = 1
-const ST_SECOND = 2
-const ST_FINAL = 3
 
-var state = 0
+var state = 2
 
 func _process(delta):
 	if $Camera/AnimationPlayer.is_playing():
 		return
 	
 	if state == 0:
-		state = 1
+		state += 1
 		$Camera/AnimationPlayer.play("first-half")
 	elif state == 1:
-		state = 2
+		state += 1
 		$Camera/AnimationPlayer.play("second-half")
 	elif state == 2:
-		state = 3
+		state += 1
+		$Camera/AnimationPlayer.play("destruction-anim")
+	elif state == 3:
+		state += 1
 		$Camera/AnimationPlayer.play("final")
